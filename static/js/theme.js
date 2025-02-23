f42838e5694d45307546fe744157ad9c1fb96265
AndriaBeridze <andria24b@gmail.com>
2025-02-23 18:35:12 -0500

    Style navbar

---
+++ static/js/theme.js
@@ -0,0 +1,33 @@
+function toggleTheme() {
+    var button = document.getElementById('theme-toggler');
+    var theme = document.body.classList.contains('light') ? 'dark' : 'light';
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+    localStorage.setItem('theme', theme);
+
+    if (theme === 'dark') {
+        button.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
+    } else {
+        button.innerHTML = '<i class="bi bi-moon-fill"></i>';
+    }
+}
+
+function applySavedTheme() {
+    var button = document.getElementById('theme-toggler');
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.classList.add(savedTheme);
+        if (savedTheme === 'dark') {
+            button.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
+        } else {
+            button.innerHTML = '<i class="bi bi-moon-fill"></i>';
+        }
+    } else {
+        // Default theme
+        document.body.classList.add('light');
+        button.innerHTML = '<i class="bi bi-moon-fill"></i>';
+    }
+}
+
+// Apply the saved theme on page load
+document.addEventListener('DOMContentLoaded', applySavedTheme);

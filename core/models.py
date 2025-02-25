e5880fd0e6ec97c94e9f34f68d39d56cf5d4c6b7
Beckham Truong <beckham@mac.mynetworksettings.com>
2025-02-24 19:14:10 -0500

    small edit

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     tmdb_id = models.IntegerField(unique=True) 
     title = models.CharField(max_length=255)

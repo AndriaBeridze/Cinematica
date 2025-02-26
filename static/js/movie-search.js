f8cd39b492bcc2229924af87096a6cc41a644553
AndriaBeridze <andria24b@gmail.com>
2025-02-26 10:57:09 -0500

    Made movies selectable

--- static/js/movie-search.js
+++ static/js/movie-search.js
@@ -3,50 +3,43 @@ let currentPage = 1; // Current page of movie results
 let totalPages = 1; // Total number of pages available
 let searchResultBar; // Declare searchResultBar globally
 
+let selectedMoviesBar;
+
+var lastQuery = '';
+var moviesSelected = [];
+
+
+function generateMovieCard(movie, movieContent = true) {
+    if (movieContent) {
+        return `<div class="card" onclick="clickAction(${movie.id}, '${movie.poster_path}')" id="tmdb-${movie.id}">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="card-content">
+                        <h3>${movie.title}</h3>
+                        <span>Vote Average: ${movie.vote_average.toFixed(3)}</span>
+                    </div>
+                </div>`;
+    } else {
+        return `<div class="card" onclick="clickAction(${movie.id}, '${movie.poster_path}')" id="tmdb-${movie.id}">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                </div>`;
+    }
+}
+
+
 // Function to fetch movies from the API
-async function fetchMovies(page, query = '', batchSize = 500) {
-    searchResultBar.innerHTML = '<div id="loading-spinner" class="loading-spinner"><i class="bi bi-arrow-clockwise"></i></div>'; // Show loading spinner
-    const movies = [];
-    const pagesToFetch = Math.ceil(batchSize / 20); // Each page contains 20 movies
+async function fetchMovies(page, query = '') {
+    const url = query
+        ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=${page}` // URL for searching movies
+        : `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=${page}`; // URL for top-rated movies
 
-    for (let i = 0; i < pagesToFetch; i++) {
-        const url = query
-            ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=${page + i}` // URL for searching movies
-            : `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=${page + i}`; // URL for top-rated movies
-
-        try {
-            showLoadingSpinner(); // Show loading spinner
-            const response = await fetch(url); // Fetch data from the API
-            const data = await response.json(); // Parse the JSON response
-            totalPages = data.total_pages; // Update the total number of pages
-            movies.push(...data.results); // Add the movies to the array
-        } catch (error) {
-            console.error("Error fetching movies:", error); // Log any errors
-            break; // Break the loop in case of error
-        } finally {
-            hideLoadingSpinner(); // Hide loading spinner
-        }
-
-        if (page + i >= totalPages) break; // Stop if there are no more pages
-    }
-
-    return movies; // Return the list of movies
-}
-
-// Function to show the loading spinner
-function showLoadingSpinner() {
-    const spinner = document.getElementById('loading-spinner');
-    console.log('loading...');
-    if (spinner) {
-        spinner.style.display = 'flex';
-    }
-}
-
-// Function to hide the loading spinner
-function hideLoadingSpinner() {
-    const spinner = document.getElementById('loading-spinner');
-    if (spinner) {
-        spinner.style.display = 'none';
+    try {
+        const response = await fetch(url); // Fetch data from the API
+        const data = await response.json(); // Parse the JSON response
+        totalPages = data.total_pages; // Update the total number of pages
+        return data.results; // Return the list of movies
+    } catch (error) {
+        console.error("Error fetching movies:", error); // Log any errors
+        return []; // Return an empty array in case of error
     }
 }
 
@@ -55,13 +48,7 @@ function appendMovies(movies) {
     movies = movies.filter(movie => movie.poster_path); // Filter out movies without a poster
     movies = movies.filter(movie => movie.vote_average > 3); // Filter out movies without a rating
     searchResultBar.innerHTML += movies.map(movie => {
-        return `<div class="card">
-                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-                    <div class="card-content">
-                        <h3>${movie.title}</h3>
-                        <span>${movie.vote_average.toFixed(3)}</span>
-                    </div>
-                </div>`;
+        return generateMovieCard(movie);
     }).join(''); // Create HTML for each movie and append it to the search result bar
 }
 
@@ -84,10 +71,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Event listener for infinite scrolling
     searchResultBar.addEventListener('scroll', () => {
-        if (searchResultBar.scrollTop + (searchResultBar.clientHeight) * 2 >= searchResultBar.scrollHeight) {
+        if (searchResultBar.scrollTop + searchResultBar.clientHeight * 2 >= searchResultBar.scrollHeight - 100) {
             if (currentPage < totalPages) { // Check if there are more pages to load
-                currentPage += Math.ceil(1000 / 20); // Increment the current page by the number of pages fetched in one batch
-                loadAndDisplayMovies(currentPage, searchInput.value); // Load and display the next batch of movies
+                currentPage++; // Increment the current page
+                loadAndDisplayMovies(currentPage, searchInput.value); // Load and display the next page of movies
             }
         }
     });
@@ -97,5 +84,37 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPage = 1; // Reset the current page to 1
         clearMovies(); // Clear the search result bar
         loadAndDisplayMovies(currentPage, searchInput.value); // Load and display movies based on the search query
+        lastQuery = searchInput.value;
     });
+
+    selectedMoviesBar = document.getElementById('selected-movies')
+    
 });
+
+// Function to handle click action on movie card
+function clickAction(id, poster_path) {
+    const movieExists = moviesSelected.some(movie => movie.id === id);
+    if (!movieExists) {
+        moviesSelected.push({
+            id: id,
+            poster_path: poster_path
+        });
+
+        selectedMoviesBar.innerHTML += generateMovieCard({
+            id: id,
+            poster_path: poster_path
+        }, false);
+    } else {
+        moviesSelected = moviesSelected.filter(movie => movie.id !== id);
+        selectedMoviesBar.innerHTML = '';
+        moviesSelected.forEach(movie => {
+            selectedMoviesBar.innerHTML += generateMovieCard(movie, false);
+        });
+    }
+}
+
+function submitMovies() {
+    console.log(moviesSelected);
+}
+
+

dd051ca8355c3589fde8186859a2d148a6e65fec
AndriaBeridze <andria24b@gmail.com>
2025-05-30 11:43:01 -0400

    Add carousel buttons in trending movies

--- static/js/trending.js
+++ static/js/trending.js
@@ -1,18 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
     trending = document.getElementById("trending");
 
+    var currentSlide = 0;
+    var prevBtn = document.querySelector(".control.prev");
+    var nextBtn = document.querySelector(".control.next");
+    var indicators = document.querySelectorAll(".indicators-container .bi");
+
+    for (let i = 0; i < indicators.length; i++) {
+        indicators[i].addEventListener("click", () => {
+            currentSlide = i;
+            showSlide(currentSlide, trending.querySelectorAll("#movie"), indicators);
+        });
+    }
+
     if (trending) {
-        let cnt = 0;
         var movies = trending.querySelectorAll("#movie");
-        console.log(movies.length);
 
         if (movies.length > 0) {
-            movies[cnt].classList.add("visible");
+            showSlide(currentSlide, movies, indicators);
             setInterval(() => {
-                movies[cnt].classList.remove("visible");
-                cnt = (cnt + 1) % movies.length;
-                movies[cnt].classList.add("visible");
+                currentSlide = (currentSlide + 1) % movies.length;
+                showSlide(currentSlide, movies, indicators);
             }, 10000);
         }
     }
+
+    prevBtn?.addEventListener("click", () => {
+        currentSlide = (currentSlide - 1 + movies.length) % movies.length;
+        showSlide(currentSlide, movies, indicators);
+    });
+
+    nextBtn?.addEventListener("click", () => {
+        currentSlide = (currentSlide + 1) % movies.length;
+        showSlide(currentSlide, movies, indicators);
+    });
 });
+
+function showSlide(index, movies, indicators) {
+    if (index < 0 || index >= movies.length) return;
+    movies.forEach(movie => movie.style.opacity = 0);
+    movies[index].style.opacity = 1;
+    indicators.forEach(indicator => {
+        indicator.classList.remove("bi-circle-fill");
+        indicator.classList.add("bi-circle");
+    });
+
+    indicators[index].classList.remove("bi-circle");
+    indicators[index].classList.add("bi-circle-fill");
+}
